8588178ffd04e6414e7227bf9e2cbf9b4fab94f3
Federico Raspa <feder@Federicos-MBP.station>
2023-03-02 00:23:39 +0100

    commenti su login_screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:the_fridge/screens/home_screen.dart';
@@ -5,22 +7,115 @@ import 'package:the_fridge/screens/home_screen.dart';
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
+  // text editing controllers
+  //Creates a controller for an editable text field.
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  double _sigmaX = 5; // from 0-10
+  double _sigmaY = 5; // from 0-10
+  double _opacity = 0.2;
+  double _width = 350;
+  double _height = 300;
+  final _formKey = GlobalKey<FormState>();
+
+  // sign user in method
+  //void signUserIn() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       body: SingleChildScrollView(
         child: Container(
-          color: const Color.fromARGB(219, 255, 226, 7),
           height: MediaQuery.of(context).size.height,
+          color: Colors.amber
           child: Stack(
             alignment: Alignment.center,
             children: [
               
-              ),
-            ],
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //pulsante per tornare indietro
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back_ios),
+                    color: Colors.white,
+                    onPressed: () {},
+                  ),
+                  //Testo LOG IN
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.18),
+                  const Text("Log In",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold)),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                  //riquadro esterno inserimento credenziali
+                  ClipRect(
+                    //effetto blur sull'immagine background
+                    child: BackdropFilter(
+                      filter:
+                          ImageFilter.blur(sigmaX: _sigmaX, sigmaY: _sigmaY),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        decoration: BoxDecoration(
+                            color: Color.fromRGBO(0, 0, 0, 1)
+                                .withOpacity(_opacity),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30))),
+                        width: MediaQuery.of(context).size.width * 0.9,
+                        height: MediaQuery.of(context).size.height * 0.63,
+                        //Container inserimento email
+                        child: Form(
+                          key: _formKey,
+                          child: Center(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                // username textfield
+                                MyTextField(
+                                  controller: usernameController,
+                                  hintText: 'Email',
+                                  obscureText: false,
+                                ),
+
+                                const SizedBox(height: 10),
+
+                                // sign in button (da aggiungere ai widget esterni)
+                                MyButton(
+                                  onTap: (() {
+                                    if (_formKey.currentState!.validate()) {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => Signup()),
+                                      );
+                                    } else {
+                                      print('not valid');
+                                    }
+                                  }),
+                                ),
+
+                                const SizedBox(height: 10),
+
+                               
+                                    ],
+                                  ),
+                                ),
+                              
+                            ),
+                          ),
+                        ),
+                      ),
+                     ],
+                    ),                  
+                ],
+              )            
           ),
         ),
-      ),
-    );
+      );
   }
 }
