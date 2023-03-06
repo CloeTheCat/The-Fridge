2760ece0c0442fca93db9cd00bdcf9168d117c66
Federico Raspa <feder@Federicos-MacBook-Pro.local>
2023-03-06 23:50:00 +0100

    q

---
+++ lib/state/button_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ButtonColor extends GetxController {
+  //final buttonGargb = [255, 255, 0, 0];
+  var a_gb = 255.obs;
+  var r_gb = 255.obs;
+  var g_gb = 0.obs;
+  var b_gb = 0.obs;
+}
